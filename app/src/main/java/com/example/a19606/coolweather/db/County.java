29cfa5d2db48162816a04c68f61a79ce6917af90
MHD_CC <944506900@qq.com>
2017-05-06 19:30:10 +0800

    加入一些配置

---
+++ app/src/main/java/com/example/a19606/coolweather/db/County.java
@@ -0,0 +1,46 @@
+package com.example.a19606.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 19606 on 2017/5/6.
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private int weatherId;
+    private int cityId;
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
